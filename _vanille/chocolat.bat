38f1e3e3963f64c2ba8a6e339620923620650087
jansson eri <linkid26@gmail.com>
2020-01-29 15:47:31 +0100

    Add files via upload

--- _vanille/chocolat.bat
+++ _vanille/chocolat.bat
@@ -42,6 +42,7 @@ cls
 call vanille.bat
 
 :cc_1
+cls
 set "errn=3"
 set "_cc1=0"
 if %width% geq %height% (
@@ -53,8 +54,8 @@ echo which cropping method do you want to use?
 echo  0 ^| centered square (default)
 echo  1 ^| square 
 echo  2 ^| ^free form (hard to use)
-set /p _cc1=""
-call :cc1_%_cc1%%ax% 2> nul
+set /p _cc=""
+call :cc%_choco%_%_cc%%ax% 2> nul
 call :err_%errn%
 goto cc_%_choco%
 
@@ -82,6 +83,7 @@ goto choco
 
 :cc1_1h
 set "errn=2"
+cls
 echo where do you want to start cropping? (from left to right)
 echo value is a single integer
 echo ex. 95 means it will start cropping 95 pixels from the left
@@ -94,11 +96,14 @@ goto choco
 
 :cc1_1v
 set "errn=2"
+cls
 echo where do you want to start cropping? (from top to bottom)
 echo value is a single integer
 echo ex. 95 means it will start cropping 95 pixels from the top
 set /p x1=""
 set /a "x2=%x1%+%width%"
+cls
+echo writing %file%....
 gifsicle --crop 0,%x1%-%width%,%x2% -i "%src%" -o "%file%"
 if exist "%file%" call :end
 call :err_%errn%
@@ -106,12 +111,15 @@ goto choco
 
 :cc1_2h
 set "errn=2"
+cls
 echo where do you want to start cropping? (top left corner)
 echo comma separated coordinates
 echo ex. 95,39 means it will start cropping 95 pixels from the left and 39 from the top
 set /p x1=""
 echo where do you want to stop cropping? (bottom right corner)
 set /p y1=""
+cls
+echo writing %file%....
 gifsicle --crop %x1%-%y1% -i "%src%" -o "%file%"
 if exist "%file%" call :end
 call :err_%errn%
@@ -120,7 +128,7 @@ goto choco
 :cc1_2v
 goto cc1_2h
 
-:cc2
+:cc_2
 set "errn=2"
 set w1=_
 set h1=_
@@ -128,24 +136,30 @@ echo set the width (default is source)
 set /p w1=""
 echo set the height (default is source)
 set /p h1=""
+cls
+echo writing %file%....
 gifsicle --no-logical-screen --resize %w1%x%h1% -i "%src%" -o "%file%"
 if exist "%file%" call :end
 call :err_%errn%
 goto choco
 
-:cc3
+:cc_3
 set "errn=3"
+cls
 echo how do you want to rotate the gif?
 echo 0 ^| rotate right (90°)
 echo 1 ^| rotate upside down (180°)
 echo 2 ^| rotate left (-90°)
-set /p _cc3=""
-call :cc3_%_cc3% 2> nul
+echo 3 ^| reset (0°)
+set /p _cc=""
+call :cc%_choco%_%_cc% 2> nul
 call :err_%errn%
 goto cc_%_choco%
 
 :cc3_0
 set "errn=2"
+cls
+echo writing %file%....
 gifsicle --rotate-90 -i "%src%" -o "%file%"
 if exist "%file%" call :end
 call :err_%errn%
@@ -153,6 +167,8 @@ goto choco
 
 :cc3_1
 set "errn=2"
+cls
+echo writing %file%....
 gifsicle --rotate-180 -i "%src%" -o "%file%"
 if exist "%file%" call :end
 call :err_%errn%
@@ -160,13 +176,25 @@ goto choco
 
 :cc3_2
 set "errn=2"
+cls
+echo writing %file%....
 gifsicle --rotate-270 -i "%src%" -o "%file%"
 if exist "%file%" call :end
 call :err_%errn%
 goto choco
 
-:cc4
+:cc3_3
+set "errn=2"
+cls
+echo writing %file%....
+gifsicle --no-rotate -i "%src%" -o "%file%"
+if exist "%file%" call :end
+call :err_%errn%
+goto choco
+
+:cc_4
 set "errn=3"
+cls
 echo how do you want to flip the gif?
 echo 0 ^| horizontally (upside-down)
 echo 1 ^| vertically (left side-right)
@@ -177,14 +205,109 @@ goto cc_%_choco%
 
 :cc4_0
 set "errn=2"
-gifsicle --rotate-270 -i "%src%" -o "%file%"
+cls
+echo writing %file%....
+gifsicle --flip-horizontal -i "%src%" -o "%file%"
+if exist "%file%" call :end
+call :err_%errn%
+goto choco
+
+:cc4_1
+set "errn=2"
+cls
+echo writing %file%....
+gifsicle --flip-vertical -i "%src%" -o "%file%"
+if exist "%file%" call :end
+call :err_%errn%
+goto choco
+
+:cc_x1
+set "errn=3"
+set "try=1"
+set "loss=25"
+set target=15
+cls
+echo how do you want to optimise the gif?
+echo 0 ^| automatically (try to reach a specific size)
+echo 1 ^| manually (set the compression level)
+set /p _cc=""
+call :cc%_choco%_%_cc% 2> nul
+call :err_%errn%
+goto cc_%_choco%
+
+:ccx1_0
+cls
+echo set the target filesize in mib (default is 15 mib)
+set /p target=""
+set /a "target=%target%*1048576"
+call :ccx1_0$
+
+:ccx1_0$
+cls
+echo attempt number %try%... (compression %loss%)
+if %try% gtr 5 echo the gif might not look great
+gifsicle -O3 --lossy=%loss% -i "%src%" -o "%file%"
+set /a "try+=1"
+if %try% gtr 8 call :end
+set /a "loss+=25"
+for %%A IN ("%file%") do set size=%%~zA
+if %size% gtr %target% (
+    goto ccx1_0$
+) else (
+    call :end
+)
+
+:ccx1_1
+cls
+echo set the compression level (default is 20, max is 200)
+set /p loss=""
+cls
+echo writing %file%....
+gifsicle -O3 --lossy=%loss% -i "%src%" -o "%file%"
+if exist "%file%" call :end
+call :err_%errn%
+goto choco
+
+:cc_x2
+set "errn=2"
+cls
+echo writing %file%....
+gifsicle --unoptimize -i "%src%" -o "%file%"
+if exist "%file%" call :end
+call :err_%errn%
+goto choco
+
+:cc_x3
+cls
+echo set the amount of colours (between 2 and 255, 256 if no transparency)
+set /p k=""
+cls
+echo writing %file%....
+gifsicle --colors %k% -i "%src%" -o "%file%"
+if exist "%file%" call :end
+call :err_%errn%
+goto choco
+
+:cc_x4
+cls
+echo set the delay between frames in hundredths of a second 
+echo at least 2 since it is the maximum widely supported timing and is roughly equal to rolling 50fps
+set /p s=""
+cls
+echo writing %file%....
+gifsicle --delay %s% -i "%src%" -o "%file%"
 if exist "%file%" call :end
 call :err_%errn%
 goto choco
 
 :end
 cls
-echo the gif has successfully been made
+if %try% gtr 8 (
+    echo the filesize has not been reached ^
+you might want to try another method like cropping or resizing
+) else (
+    echo the gif has successfully been made
+)
 pause
 cls
 goto choco
@@ -201,10 +324,13 @@ cls
 echo error %errn%, file not found or nothing was selected please retry%NL%
 goto in
 
+:err_2
+cls
+echo error %errn%, something wrong occured please retry, maybe you input an incorrect value%NL%
+goto in
+
 :err_3
 cls
 echo error %errn%, sorry this value is invalid please retry%NL%
 goto :eof 
 #>
-
-ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 .\e.gif
